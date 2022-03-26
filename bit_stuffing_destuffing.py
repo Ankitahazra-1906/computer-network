a8fef4d70b7f0948bbbf1c564ae0c2dbaf747f5c
Ankita Hazra <ankitahazra2000@gmail.com>
2022-03-26 20:12:36 +0530

    Update bit_stuffing_destuffing.py

--- bit_stuffing_destuffing.py
+++ bit_stuffing_destuffing.py
@@ -1,22 +1,27 @@
 a=input("Enter the sequence: ")
+flag=[1,1,1,1,1]
+z="".join(map(str,flag))
+print("Flag: ",z)
 b=list(a)
+d=[]
 count=0
-for i in range(0,4):
-    if(b[i]!='1'):
-        print('invalid')
-        exit()
+e=0
+
 #Bit Stuffing
-for i in range(5,len(b)):
+for i in range(0,len(b)):
     if(b[i]=='1'):
         count=count+1
         if(count==5):
             b.insert(i+1,'0')
+            d.append(i+1)
+            e+=1
     else:
         count=0
 print(b)
 x="".join(map(str,b))
+
 #Bit Destuffing
-for i in range(5,len(b)-1):
+for i in range(0,len(b)-4):
     if(b[i]=='1'):
         count=count+1
         if(count==5):
@@ -25,5 +30,12 @@ for i in range(5,len(b)-1):
         count=0
 print(b)
 y="".join(map(str,b))
-print("Bit stuffing: ",x)
-print("Bit destuffing: ",y)
+
+p=(str(z)+str(x))
+q=(str(z)+str(y))
+print("The index where the bits are stuffed: ",d)
+print("The no. of bits are stuffed: ",e)
+print("Bit stuffing frame: ",p)
+print("Bit destuffing frame: ",q)
+
+
